77ebac6b091b0f6b7ebb8092962003c551b1641f
mye785c83 <mye785c83@gmail.com>
2021-01-05 12:07:52 +0900

    complete history table design

--- lib/views/history.dart
+++ lib/views/history.dart
@@ -6,6 +6,21 @@ class HistoryTab extends StatefulWidget {
 }
 
 class _HistoryTabState extends State<HistoryTab> {
+  final tableTitleStyle = TextStyle(
+      fontFamily: "AppleSDGothicNeo",
+      fontWeight: FontWeight.w500,
+      color: Colors.black,
+      fontSize: 16,
+      height: 1.8
+  );
+
+  final tableDetailStyle = TextStyle(
+    fontFamily: "AppleSDGothicNeo",
+    fontWeight: FontWeight.w300,
+    color: Colors.black,
+    fontSize: 12,
+  );
+
   final BoxDecoration _tableDecoration = BoxDecoration(
       borderRadius: BorderRadius.circular(15),
       gradient: LinearGradient(
@@ -38,10 +53,99 @@ class _HistoryTabState extends State<HistoryTab> {
         alignment: Alignment.center,
         children: [
           Container(
-            decoration: _tableDecoration,
-          )
+              width: _width*0.9,
+              height: _height*0.8,
+              decoration: _tableDecoration,
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: _height*0.01,
+                  ),
+                  Table(
+                    defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                    columnWidths: {
+                      0: FractionColumnWidth(.27),
+                      1: FractionColumnWidth(.39),
+                      2: FractionColumnWidth(.34),
+                    },
+                    children: [
+                      // title of each columns
+                      TableRow(
+                        // Title of each Column
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(0, 0, 0, 0),
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Color.fromARGB(255, 35, 37, 43),
+                                style: BorderStyle.solid,
+                              ),
+                            ),
+                          ),
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 1.5),
+                              child: Text("Date", style: tableTitleStyle, textAlign: TextAlign.center,),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 1.5),
+                              child: Text("TimeExceeded", style: tableTitleStyle, textAlign: TextAlign.center,),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 1.5),
+                              child: Text("Penalty", style: tableTitleStyle, textAlign: TextAlign.center,),
+                            ),
+                          ]
+                      ),
+                    ],
+                  ),
+                  Flexible(
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Table(
+                        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                        columnWidths: {
+                          0: FractionColumnWidth(.27),
+                          1: FractionColumnWidth(.39),
+                          2: FractionColumnWidth(.34),
+                        },
+                        children: [
+                          for(int i=0; i<20; i++)
+                            dataRow(),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: _height*0.01,
+                  ),
+                ],
+              ),
+          ),
         ],
       ),
+    );
+  }
+
+  TableRow dataRow(){
+    return TableRow(
+        decoration: BoxDecoration(
+            color: Color.fromARGB(0, 0, 0, 0),
+        ),
+        children: [
+          Padding(
+            padding: EdgeInsets.all(3.5),
+            child: Text("21-02-04", style: tableDetailStyle, textAlign: TextAlign.center,),
+          ),
+          Padding(
+            padding: EdgeInsets.all(3.5),
+            child: Text("76 sec", style: tableDetailStyle, textAlign: TextAlign.center,),
+          ),
+          Padding(
+            padding: EdgeInsets.all(3.5),
+            child: Text("00,000", style: tableDetailStyle, textAlign: TextAlign.center,),
+          ),
+        ]
     );
   }
 }
