128400a34863ff669116be81caea35e39e4a316a
mye785c83 <mye785c83@gmail.com>
2021-01-19 15:00:33 +0900

    change a way of using openbanking API to using django server

--- lib/api.dart
+++ lib/api.dart
@@ -1,16 +1,3 @@
-import 'package:oauth2_client/oauth2_client.dart';
+const String url_main = 'http://13.209.74.234:5000/';
 
-const String customScheme = "https";
-const String customHost = "www.alarm-banktransfer.com";
-const String pathPrefix = "/redirecturi";
-const String clientID = "c8efeb93-515a-42c3-9d2d-542817f805ae";
-const String clientSecret = "475b94e3-a4da-47d0-9bbe-c83127e6a1fe";
-const String redirectUri = 'https://www.alarm-transfer.com/redirect-uri';
-
-const url_authorize = "https://openapi.openbanking.or.kr/oauth/2.0/authorize";
-
-OAuth2Client myOAuth2Client = OAuth2Client(
-    customUriScheme: 'https',
-    redirectUri: redirectUri,
-    authorizeUrl: redirectUri + '/authorize',
-    tokenUrl: redirectUri + '/token');
+const String url_authorize = url_main + 'authorize/';
