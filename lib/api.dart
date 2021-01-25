4b0824ee1112d8d871b33d2906a597813d2f4894
hyerin <mye785c83@gmail.com>
2021-01-25 15:04:23 +0900

    openbanking api authorize

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,4 @@
 const String url_main = 'http://13.209.74.234:5000/';
+const String url_local = 'http://192.168.222.1/';
 
-const String url_authorize = url_main + 'authorize/';
+const String url_authorize = url_main + 'connect/';
--- lib/open_banking.dart
+++ lib/open_banking.dart
@@ -6,12 +6,46 @@ import 'dart:convert';
 import 'api.dart';
 import 'dart:math';
 
-Future<String> getOAuth() async {
+const String clientID = 'c8efeb93-515a-42c3-9d2d-542817f805ae';
+const String clientSecret = '475b94e3-a4da-47d0-9bbe-c83127e6a1fe';
+
+Future<String> getOAuthfromServer() async {
   try {
     final response = await http.get(url_authorize);
     print("get response");
     if (response.statusCode == 200) {
       var rawData = response.body;
+      print(rawData);
+      return rawData;
+    }
+  } catch (error) {
+    print(error);
+    return "no/network";
+  }
+}
+
+Future<String> getOAuth() async {
+  print('request to api directly');
+  Map<String, dynamic> formdata = {
+    'response_type': 'code',
+    'client_id': clientID,
+    'redirect_uri': 'http://13.209.74.234:5000/authorize',
+    'scope': 'login inquiry transfer',
+    'state': Random().nextInt(32),
+    'auth_type': '0',
+    'lang': 'kor',
+    'cellphone_cert_yn': 'Y',
+    'authorized_cert_yn': 'N',
+    'account_hold_auth_yn': 'N'
+  };
+  try {
+    final response = await Dio().get(
+      'https://testapi.openbanking.or.kr/oauth/2.0/authorize',
+      queryParameters: formdata
+    );
+    print("get response");
+    if (response.statusCode == 200) {
+      var rawData = response.data;
       print(rawData);
       return rawData;
     }
